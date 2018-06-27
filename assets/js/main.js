60c704ddb5ea874a8ece3a19791b8153515dfae2
SamarthaKV29 <samarthakv29@gmail.com>
2018-06-27 11:17:52 -0400

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,39 +3,24 @@ const mobile = {
     init_flag: false,
 
     init: () => {
-        $(window).on('resize', function () {
+        const wid = $(window).width();
+        if (!mobile.init_flag && wid >= 450)
+            mobile.setup();
+        if (wid <= 450)
+            mobile.destroy();
 
-            const width = $(window).width();
-
-            if (width <= 450) {
-
-                if (!mobile.init_flag) {
-
-                    mobile.setup();
-
-                } else {
-
-                    mobile.update();
-                }
-
-            } else {
-
-                if (mobile.init_flag) {
-
-                    mobile.destroy();
-                }
-            }
-        });
     },
 
     setup: () => {
+
+        mobile.init_flag = true;
         mobile.controller = new ScrollMagic.Controller({
             globalSceneOptions: {
                 triggerHook: 'onEnter'
             }
         });
         $(".row").each((idx, ele) => {
-            ele.scene = new ScrollMagic.Scene({
+            var scene = new ScrollMagic.Scene({
                 x: 1000 * idx * Math.pow(-1, idx),
                 triggerElement: ele,
                 duration: 400,
@@ -46,10 +31,10 @@ const mobile = {
                 'border-radius': 0,
                 'font-size': 8,
                 'box-shadow': '1px 1px 1000px 100px black, 1px 1px 10px 4px black inset'
-            })).addTo(mobile.controller);
+            })).addTo(mobile.controller).addIndicators();
         });
         $(".step").each((idx, ele) => {
-            ele.scene = new ScrollMagic.Scene({
+            var scene = new ScrollMagic.Scene({
                 triggerElement: ele,
                 duration: 500,
                 reverse: true
@@ -58,30 +43,20 @@ const mobile = {
                 filter: 'blur(5px)',
             })).addTo(mobile.controller);
         });
-        $('.fix').each((idx, ele) => {
-            ele.scene = new ScrollMagic.Scene({
-                triggerElement: $('.row')[0],
-                duration: $(document).height(),
-            }).setTween(new TweenMax.from('.fix', 1, {
-                opacity: 0
-            })).addTo(mobile.controller);
-        });
+        var scene = new ScrollMagic.Scene({
+            triggerElement: $('.row')[0],
+            duration: $(document).height(),
+        }).setTween(new TweenMax.from('.fix', 1, {
+            opacity: 0
+        })).addTo(mobile.controller);
     },
 
-    update: () => {
-        if (mobile.init_flag) {
-            $('.step, .row, .fix').each((idx, ele) => {
-                ele.scene.duration(ele.clientHeight);
-            });
-        }
-    },
     destroy: function () {
-        mobile.controller.destroy(true);
+        mobile.controller = null;
         mobile.init_flag = false;
     }
 
 };
-
 $(document).ready(() => {
     mobile.init();
 });
